1e8698a2019445631506aec690ce304186e41406
tanuj2062 <tanujbordikar@gmail.com>
2023-07-02 20:53:15 +0530

    blog page commit

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -1,6 +1,7 @@
 import { client } from "@/app/lib/sanity";
 import { urlFor } from "@/app/lib/sanityImageUrl";
 import { PortableText } from "@portabletext/react";
+import Layout from '@/components/Layout';
 import Image from "next/image";
 import "../../../styles/main.css";
 
@@ -30,36 +31,38 @@ export default async function SlugPage({ params }) {
   };
 
   return (
-    <div className="text-align-center xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-      <header className="pt-6 xl:pb-6">
-        <div className="space-y-1 text-center">
-          <div className="space-y-10">
+    <Layout>
+      <div className="text-align-center xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+        <header className="pt-6 xl:pb-6">
+          <div className="space-y-1 text-center">
+            <div className="space-y-10">
+              <div>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                  {new Date(data._createdAt).toISOString().split("T")[0]}
+                </p>
+              </div>
+            </div>
+
             <div>
-              <p className="text-base font-medium leading-6 text-teal-500">
-                {new Date(data._createdAt).toISOString().split("T")[0]}
-              </p>
+              <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                {data.title}
+              </h1>
+              <a className="" href="/blogs">Back</a>
             </div>
           </div>
+        </header>
 
-          <div>
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-              {data.title}
-            </h1>
-            <a className="" href="/blogs">Back</a>
-          </div>
-        </div>
-      </header>
-
-      <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
-        <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-          <div className="centerAlign prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
-            <PortableText
-              value={data.content}
-              components={PortableTextComponent}
-            />
+        <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
+          <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="centerAlign prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
+              <PortableText
+                value={data.content}
+                components={PortableTextComponent}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
